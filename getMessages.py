1de934e3c9737bdc7ecd4fa749d3ad322b7d88e5
Yeh <yeh@whoosh.cn>
2016-03-19 02:59:31 +0800

    try to unpack data

--- getMessages.py
+++ getMessages.py
@@ -1,11 +1,13 @@
 import socket
 import time
+from struct import unpack
 
 port = 8888
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.bind(("",port))
 print('waiting on port:', port)
 while True:
-	data, addr = s.recvfrom(1024)
-	print("DATA:", data, addr)
-	time.sleep(100)
+	data, addr = s.recvfrom(12)
+	data = unpack("2s5h", data)
+	print("Data:", data, "From:", addr)
+	time.sleep(1)
