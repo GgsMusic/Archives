ea09bc673131590486f8871546d6f3c55d7665c6
Yeh <yeh@whoosh.cn>
2016-04-07 16:17:03 +0800

    almost complete

--- receiveUDP.py
+++ receiveUDP.py
@@ -5,24 +5,25 @@ from pythonosc import udp_client
 
 host = ''
 port = 8888
-bufsiz = 1024
+bufsiz = 128
 addr = (host,port)
 
 udpSerSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udpSerSock.bind(addr)
 
+print("osc messages are sending to port 8086")
 while True:
 	data,addr = udpSerSock.recvfrom(bufsiz)
 	value = data.split(b".")
-	client = udp_client.UDPClient("localhost", 8086)
+	client = udp_client.UDPClient("localhost", 9999)
 	for i in range(len(value)):
 		if len(value[i]) ==  27:
 			value[i] = value[i].split(b",")
 			osclist = [int(field) for field in value[i][1:]]
-			msg = osc_message_builder.OscMessageBuilder(address = "/YY")
+#			print(osclist)
+			msg = osc_message_builder.OscMessageBuilder(address = "/YYSeesaw")
 			for n in range(5):
 			  msg.add_arg(osclist[n])
 			msg = msg.build()
 			client.send(msg)
-			print(osclist)
 	time.sleep(0.1)
